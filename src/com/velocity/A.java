f10519b937762a79702138a6826f263675c441d2
swapnilgawai <sjgawai555@gmail.com>
2023-03-18 14:15:36 +0530

    first commit:added stringprogram code

---
+++ src/com/velocity/A.java
@@ -0,0 +1,13 @@
+package com.velocity;
+
+//reverse the string 
+public class A {
+
+	public static void main(String[] args) {
+		StringBuffer str=new StringBuffer("Java");
+		System.out.println(str.reverse());
+	}
+}
+
+
+ 
