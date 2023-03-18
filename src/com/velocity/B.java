3a48c02e58550c1de577f17cde7f9cde19ba5563
swapnilgawai <sjgawai555@gmail.com>
2023-03-18 18:14:32 +0530

    first commit: added stringbuilder code for reverse string

---
+++ src/com/velocity/B.java
@@ -0,0 +1,10 @@
+package com.velocity;
+
+public class B {
+
+	public static void main(String[] args) {
+		StringBuilder str = new StringBuilder("Program");
+		StringBuilder sb = str.reverse();
+		System.out.println(sb);
+	}
+}
